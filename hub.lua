c2e5a0c4fe09e4fd9fe750f0682091221bd8fd0a
angercc <87264662+angercc@users.noreply.github.com>
2021-11-18 17:37:02 +0000

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,102 +1,26 @@
 {
-  "4.1": [
-      25,
+  "Default": [
+      1,
       "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
   ],
-  "4.0": [
-      24,
+  "Blood": [
+      2,
       "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
   ],
-  "3.9": [
-      23,
+  "Blue": [
+      3,
       "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
   ],
-  "3.7": [
-      22,
+  "Coastified": [
+      4,
       "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
   ],
-  "3.6": [
-    21,
-    "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
-  ],
-  "1.9": [
-    6,
-    "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
-  ],
-  "2.1": [
-    8,
-    "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
-  ],
-  "3.0": [
-    17,
-    "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
-  ],
-  "1.8": [
+  "Camo": [
     5,
     "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
   ],
-  "2.9": [
-    16,
+  "Oceanic": [
+    6,
     "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
   ],
-  "1.5": [
-    2,
-    "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
-  ],
-  "2.5": [
-    12,
-    "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
-  ],
-  "2.4": [
-    11,
-    "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
-  ],
-  "2.8": [
-    15,
-    "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
-  ],
-  "0.1": [
-    1,
-    "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
-  ],
-  "3.1": [
-    18,
-    "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
-  ],
-  "2.6": [
-    13,
-    "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
-  ],
-  "1.6": [
-    3,
-    "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
-  ],
-  "1.7": [
-    4,
-    "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
-  ],
-  "2.7": [
-    14,
-    "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
-  ],
-  "2.2": [
-    9,
-    "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
-  ],
-  "3.3": [
-    20,
-    "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
-  ],
-  "2.0": [
-    7,
-    "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
-  ],
-  "2.3": [
-    10,
-    "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
-  ],
-  "3.2": [
-    19,
-    "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
-  ]
 }
