07597beba48517f90937cf622112146aa1c4b1be
angercc <87264662+angercc@users.noreply.github.com>
2021-11-19 23:04:14 +0000

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,54 +1,22 @@
 {
-    "Themes": {
-    "Default": {
-        "ThemeIndex": 1,
-        "Primary": [41, 53, 68],
-        "Secondary": [35, 47, 62],
-        "Tertiary": [28, 41, 56],
-        "Quaternary": [18, 98, 159]
-    },
-    "Dark": {
-        "ThemeIndex": 2,
-        "Primary": [25, 25, 25],
-        "Secondary": [30, 30, 30],
-        "Tertiary": [35, 35, 35],
-        "Quaternary": [40, 40, 40]
-    },
-    "Tangy": {
-        "ThemeIndex": 3,
-        "Primary": [47, 55, 65],
-        "Secondary": [60, 92, 125],
-        "Tertiary": [40, 40, 40],
-        "Quaternary": [90, 165, 173]
-    },
-        "Green": {
-            "ThemeIndex": 4,
-            "Primary": [86, 76, 251],
-            "Secondary": [38, 45, 71],
-            "Tertiary": [38, 45, 71],
-            "Quaternary": [26, 32, 58]
-        },
-        "Light": {
-            "ThemeIndex": 5,
-            "Primary": [150, 150, 150],
-            "Secondary": [200, 200, 200],
-            "Tertiary": [225, 225, 225],
-            "Quaternary": [207, 207, 207]
-        },
-        "Synapse": {
-            "ThemeIndex": 6,
-            "Primary": [46, 48, 43],
-            "Secondary": [36, 38, 35],
-            "Tertiary": [24, 24, 24],
-            "Quaternary": [13, 15, 12]
-        },
-        "Midnight": {
-            "ThemeIndex": 7,
-            "Primary": [166, 71, 214],
-            "Secondary": [36, 28, 41],
-            "Tertiary": [74, 58, 84],
-            "Quaternary": [36, 28, 41]
-        
-    }
-}
-}
+    "Blood": [
+        2,
+        "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
+    ],
+    "Blue": [
+        3,
+        "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
+    ],
+    "Coastified": [
+        4,
+        "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
+    ],
+    "Camo": [
+      5,
+      "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
+    ],
+    "Oceanic": [
+      6,
+      "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
+    ]
+  }
