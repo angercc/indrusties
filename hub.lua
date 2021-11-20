e9ce2481e8010590e9b792ba10c860f44f7513b6
angercc <87264662+angercc@users.noreply.github.com>
2021-11-20 01:02:08 +0000

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,10 +1,26 @@
 {
     "Default": [
         1,
-        "https://raw.githubusercontent.com/angercc/aztup/main/azttp.lua"
-    ]
-     "dwasd": [
+        "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
+    ],
+    "Blood": [
         2,
-        "https://raw.githubusercontent.com/angercc/aztup/main/azttp.lua"
+        "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
+    ],
+    "Blue": [
+        3,
+        "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
+    ],
+    "Coastified": [
+        4,
+        "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
+    ],
+    "Camo": [
+      5,
+      "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
+    ],
+    "Oceanic": [
+      6,
+      "https://raw.githubusercontent.com/angercc/silentaim2/main/hub.lua"
     ]
   }
