d8974d620c0bd920ffa016c0d5deb155b631ca3f
angercc <87264662+angercc@users.noreply.github.com>
2021-11-20 01:01:03 +0000

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -3,4 +3,8 @@
         1,
         "https://raw.githubusercontent.com/angercc/aztup/main/azttp.lua"
     ]
+     "dwasd": [
+        2,
+        "https://raw.githubusercontent.com/angercc/aztup/main/azttp.lua"
+    ]
   }
